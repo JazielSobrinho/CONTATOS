f98ecedbbb9790787a601d96cce584d1382a0798
JazielSobrinho <jaziel881@gmail.com>
2015-11-10 07:45:49 -0200

    novo menu para segunda activity

--- app/src/main/java/br/com/jaziel/contatos/Main2Activity.java
+++ app/src/main/java/br/com/jaziel/contatos/Main2Activity.java
@@ -1,13 +1,10 @@
 package br.com.jaziel.contatos;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -93,7 +90,7 @@ public class Main2Activity extends AppCompatActivity {
 
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
         return true;
     }
 

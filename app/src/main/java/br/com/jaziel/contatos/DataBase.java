2d1f2e2976162bf6a49b0faea76cc0f89de9516b
JazielSobrinho <jaziel881@gmail.com>
2015-11-10 09:45:17 -0200

    Merge remote-tracking branch 'github/master'
    Conflicts:
    	app/src/main/res/menu/menu_main2.xml

---
+++ app/src/main/java/br/com/jaziel/contatos/DataBase.java
@@ -0,0 +1,27 @@
+package br.com.jaziel.contatos;
+
+/**
+ * Created by LAB56 on 10/11/2015.
+ */
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataBase extends SQLiteOpenHelper {
+
+    public DataBase(Context context) {
+        super(context, "AGENDA", null, 1);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
